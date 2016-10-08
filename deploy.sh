60b22eece4649d45e4fb55ff140b97a337ac4b9f
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-10-08 18:44:44 +0700

    rebuilding site Sat Oct  8 18:44:44 ICT 2016

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public https://github.com/vietch2612/qablog.git gh-pages
+git subtree push --prefix=public https://github.com/vietch2612/qablog.git master 
